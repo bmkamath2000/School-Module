a1b0cafbd1cfd80582a799346cb939c565d6b50f
bmkamath2000 <bmkamath2000@gmail.com>
2019-12-15 13:08:30 +0530

    Added React-router-dom with routing-v4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 import './App.css';
 import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
+import Home from './Components/Home';
 import Body1 from './Components/Body1';
 import Footer from './Components/Footer';
 class App extends React.Component {
@@ -14,14 +20,47 @@ class App extends React.Component {
  {
   return (
     <div >
+      <Router>
       <Header/>
       <div className="partition-div">
       <div className="sidebar-container">
-      <Sidebar/>
+      <div className="sidebar">
+      
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        
       </div>
-      <Body1/>
+    
+      </div>
+      </div>
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            <Body1 />
+          </Route>
+        </Switch>
       </div>
       <Footer/>
+      </Router>
     </div>
   );
 }
